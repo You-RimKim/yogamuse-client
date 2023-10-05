5b6240504d62208915bbcaeff72193008ebaf3d0
You-Rim Kim <yourimykim@gmail.com>
2023-10-05 15:14:24 +0200

    tried poses

--- src/components/AddPose.jsx
+++ src/components/AddPose.jsx
@@ -1,101 +1,106 @@
 import { useState } from "react";
-import axios from "axios";
 import PropTypes from "prop-types";
-
-const API_URL = "http://localhost:5005";
-
 
 function AddPose(props) {
   // const [id, setId] = useState("");
-  const [english_name, setenglish_name] = useState("");
-  const [sanskrit_name, setsanskrit_name] = useState("");
-  const [pose_description, setpose_description] = useState("");
-  const [pose_benefits, setpose_benefits] = useState("");
-  const [url_png, seturl_png] = useState("");
+  const { onPoseAdded, 
+    english_name, 
+    setenglish_name,
+    sanskrit_name, 
+    setsanskrit_name,
+    pose_description,
+    setpose_description, 
+    pose_benefits,
+    setpose_benefits,
+    url_png,
+    seturl_png } = props;
 
-  
   const handleSubmit = (e) => {      
     e.preventDefault();
 
-    // We need the category id when creating the new pose
-    const { favoritesId } = props;
-    const requestBody = { 
-      english_name, 
-      sanskrit_name,
-      pose_description,
-      pose_benefits,
-      url_png,
-     };
- 
-    axios
-      .post(`${API_URL}/api/my-favorites/${favoritesId}/add-pose`, 
-      requestBody, 
-        { headers: {
-          Authorization: `Bearer ${getToken()}`,
-        },
-      })
-      .then((response) => {
-        // Reset the state to clear the inputs
-        setenglish_name("");
-        setsanskrit_name("");
-        setpose_description("");
-        setpose_benefits("");
-        seturl_png("");
-      
-        //props.refreshCategory();
-        props.refreshFavorite();
-      })
-      .catch((error) => console.log(error));
-  };
+    onPoseAdded();
 
-  
-  return (
-    <div className="AddPose">
-      <h3>Add New Pose</h3>
-      
-      <form onSubmit={handleSubmit}>
-        <label>English Name:</label>
-        <input
-          type="text"
-          name="english_name"
-          value={english_name}
-          onChange={(e) => setenglish_name(e.target.value)}
-        />
-
-        <label>Sanskrit Name:</label>
-        <textarea
-          type="text"
-          name="sanskrit_name"
-          value={sanskrit_name}
-          onChange={(e) => setsanskrit_name(e.target.value)}
-        />
-
-        <label>Pose Description:</label>
-        <textarea
-          type="text"
-          name="pose_description"
-          value={pose_description}
-          onChange={(e) => setpose_description(e.target.value)}
-        />
-
-        <label>Pose Benefits:</label>
-        <textarea
-          type="text"
-          name="pose_benefits"
-          value={pose_benefits}
-          onChange={(e) => setpose_benefits(e.target.value)}
-        />
-
-        {/* url_png here */}
-
-        <button type="submit">Add Pose</button>
-      </form>
-    </div>
-  );
+    setenglish_name("");
+    setsanskrit_name("");
+    setpose_description("");
+    setpose_benefits("");
+    seturl_png("");
+    };
 }
 
 AddPose.propTypes = {
   onPoseAdded: PropTypes.func.isRequired,
+  english_name: PropTypes.string.isRequired,
+  setenglish_name: PropTypes.string.isRequired,
+  sanskrit_name: PropTypes.string.isRequired,
+  setsanskrit_name: PropTypes.string.isRequired,
+  pose_description: PropTypes.func.isRequired,
+  setpose_description: PropTypes.func.isRequired,
+  pose_benefits: PropTypes.func.isRequired,
+  setpose_benefits: PropTypes.func.isRequired,
+  url_png: PropTypes.func.isRequired,
+  seturl_png: PropTypes.func.isRequired,
+
+
 };
 
 export default AddPose;
+
+
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import { Link, useParams } from "react-router-dom";
+// import AddPose from "../components/AddPose";
+// import PoseCard from "../components/PoseCard";
+
+// const API_URL = "http://localhost:5005"; 
+
+
+// function AddNewPose (props) {
+//   const [newPose, setNewPose] = useState(null);
+//   const { poseId } = useParams();
+
+//   const getPose = () => {      
+//     const storedToken = localStorage.getItem("authToken");
+//   axios
+//       .get(
+//         `${API_URL}/api/categories/${poseId}`,
+//         { headers: { Authorization: `Bearer ${storedToken}` } }
+//       )
+//       .then((response) => {
+//         const onePose = response.data;
+//         setNewPose(onePose);
+//       })
+//       .catch((error) => console.log(error));
+//   };
+
+//   useEffect(()=> {                   
+//     getPose();
+//   }, [poseId] );
+
+  
+//   return (
+//     <div className="PoseDetails">
+      
+//       {newPose && (
+//         <>
+//           <h1>{newPose.category_name}</h1>
+//           <p>{newPose.category_description}</p>
+//         </>
+//       )}
+
+//       <AddPose refreshNewPose={getPose} poseId={poseId} favoritesId={favoritesId} />
+
+//       { newPose && newPose.poses.map((pose) => (
+//         <PoseCard key={pose._id} {...pose} /> 
+//       ))} 
+      
+//       <Link to={"/my-favorites"}>
+//         <button>Go to my favorites</button>
+//       </Link>      
+      
+//     </div>
+//   );
+// }
+ 
+// export default AddNewPose;
--- src/pages/FavoriteDetailsPage.jsx
+++ src/pages/FavoriteDetailsPage.jsx
@@ -1,18 +1,182 @@
+// import axios from "axios";
+// import { useContext, useEffect, useState } from "react";
+// import { Link, useParams, useNavigate } from "react-router-dom";
+// import { AuthContext } from "../context/auth.context";
+// import AddPose from "../components/AddPose";
+
+// export default function FavoriteDetailsPage() {
+//   const [favorite, setFavorite] = useState(null);
+//   const [categories, setCategories] = useState([]);
+//   const [isAddingPose, setIsAddingPose] = useState(false);
+//   const { favoritesId } = useParams();
+//   const {getToken} = useContext(AuthContext)
+//   const navigate = useNavigate();
+
+// useEffect(() => {
+//   const fetchFavorite = async () => {
+//     try {
+//       const response = await fetch(
+//         `http://localhost:5005/api/my-favorites/${favoritesId}`,
+//         {
+//           method: 'GET',
+//           headers: {
+//             Authorization: `Bearer ${getToken()}`
+//           }
+//         }
+//       );
+//       const data = await response.json()
+//       setFavorite(data)
+
+//       if (!response.ok) {
+//         throw new Error("Failed to fetch favorites");
+//       }
+//       // const favoriteData = await response.json();
+//     } catch (error) {
+//       console.error("Error fetching favorites:", error);
+//     }
+//   };
+
+//   fetchFavorite();
+// }, []);
+
+// // Function to toggle the Add Pose form visibility
+// const toggleAddPoseForm = () => {
+//   setIsAddingPose(!isAddingPose);
+// };
+
+// const handleAddPose = async () => {
+//   try {
+//     const storedToken = localStorage.getItem("authToken");
+//     const response = await axios.post(
+//       `http://localhost:5005/api/my-favorites/${favoritesId}`,
+
+//       { 
+//         english_name,
+//         sanskrit_name,
+//         pose_description,
+//         pose_benefits,
+//         url_png, },
+//       {
+//         headers: { Authorization: `Bearer ${storedToken}` },
+//       }
+//     );
+
+//     const newFavoritePose = response.data;
+//     setFavoritePose((prevFavoritePose) => [...prevFavoritePose, newFavoritePose]);
+
+//     toggleAddPoseForm();
+//     fetchFavorites()
+//   } catch (error) {
+//     console.error("Error adding favorite pose:", error);
+//   }
+// };
+
+// const handleDeleteCategory = () => {
+//   const storedToken = getToken();
+
+//   axios
+//     .delete(`http://localhost:5005/api/my-favorites/${favoritesId}`, {
+//       headers: {
+//         Authorization: `Bearer ${storedToken}`,
+//       },
+//     })
+//     .then((response) => {
+//       console.log('Category deleted:', response.data);
+
+//       setCategories((prevCategories) =>
+//       prevCategories.filter(
+//         (category) => category.category_id !== favoritesId
+//       )
+//     );
+
+//       navigate( '/my-favorites' );
+//     })
+//     .catch((error) => {
+//       console.error('Error deleting category:', error);
+//     });
+// }
+  
+
+//     return (
+//         <div className="FavoriteCategory">
+//         <h2>Favorite Categories</h2>
+
+//         <Link to="/my-favorites/edit/:favoritesId">
+//         <button>Update category</button>
+//         </Link>
+
+//         <Link to="/add-pose/:poseId">
+//         <button>Add a pose</button>
+//         </Link>
+
+//         <button onClick={() => handleDeleteCategory(favorite.category_id)}>
+//           Delete Category
+//         </button>
+
+//         {/* <button onClick={toggleAddPoseForm}>
+//           "Add Pose"
+//         </button> */}
+
+//         {isAddingPose && (
+//           <AddPose
+//             onPoseAdded={handleAddPose}
+//             // Pass any necessary props to the AddPose component
+//           />
+//         )}
+
+//         {favorite && (
+//             <>
+//             <h3>{favorite.category_name}</h3>
+//             <p>{favorite.category_description}</p>
+//             <h4>Poses in {favorite.category_name}</h4>
+//             <ul>
+//                 {favorite.poses &&
+//                 favorite.poses.map((pose) => (
+//                     <li key={pose.id}>
+//                     <h5>{pose.english_name}</h5>
+//                     <h5>{pose.sanskrit_name}</h5>
+//                     <img src={pose.url_svg} alt={pose.english_name} />
+//                     <p>{pose.pose_description}</p>
+//                     <p>{pose.pose_benefits}</p>
+//                     </li>
+//                 ))}
+//             </ul>
+//             </>
+//         )}
+
+        
+//         </div>
+//     );
+//     }
+
+
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 import AddPose from "../components/AddPose";
+import AddNewPose from "./AddNewPose";
 
 export default function FavoriteDetailsPage() {
   const [favorite, setFavorite] = useState(null);
   const [categories, setCategories] = useState([]);
+  const [showAddPoseForm, setShowAddPoseForm] = useState(false);
   const [isAddingPose, setIsAddingPose] = useState(false);
-  const { favoritesId } = useParams();
+  const [english_name,setEnglishName]  = useState("")
+  const [sanskrit_name,setSanskritName]  = useState("")
+  const [pose_description,setPoseDescription]  = useState("")
+  const [pose_benefits,setPoseBenefits]  = useState("")
+  const [url_png,setUrlPng]  = useState("")
+
+  const { favoritesId, poseId } = useParams();
   const {getToken} = useContext(AuthContext)
   const navigate = useNavigate();
 
 useEffect(() => {
+  fetchFavorite();
+}, []);
+
+
   const fetchFavorite = async () => {
     try {
       const response = await fetch(
@@ -24,31 +188,22 @@ useEffect(() => {
           }
         }
       );
-      const data = await response.json()
-      setFavorite(data)
 
-      if (!response.ok) {
-        throw new Error("Failed to fetch favorites");
-      }
-      // const favoriteData = await response.json();
+      setFavorite(response.data);
     } catch (error) {
-      console.error("Error fetching favorites:", error);
+      console.error("Error fetching favorite poses:", error);
     }
   };
 
-  fetchFavorite();
-}, []);
-
-// Function to toggle the Add Pose form visibility
 const toggleAddPoseForm = () => {
-  setIsAddingPose(!isAddingPose);
+  setShowAddPoseForm(!setShowAddPoseForm);
 };
 
-const handleAddPose = async () => {
+const handleAddFavoritePose = async () => {
   try {
     const storedToken = localStorage.getItem("authToken");
     const response = await axios.post(
-      `http://localhost:5005/api/my-favorites/${favoritesId}`,
+      `http://localhost:5005/api/my-favorites/${favoritesId}/poses`,
 
       { 
         english_name,
@@ -62,7 +217,12 @@ const handleAddPose = async () => {
     );
 
     const newFavoritePose = response.data;
-    setFavoritePose((prevFavoritePose) => [...prevFavoritePose, newFavoritePose]);
+
+    setFavorite((prevFavorite) => ({
+        ...prevFavorite,
+        poses: [...prevFavorite.poses, newFavoritePose],
+      }));
+
 
     toggleAddPoseForm();
     fetchFavorites()
@@ -97,54 +257,111 @@ const handleDeleteCategory = () => {
 }
   
 
-    return (
-        <div className="FavoriteCategory">
-        <h2>Favorite Categories</h2>
+return (
+  <div className="FavoriteCategory">
+    <h2>Favorite Categories</h2>
 
-        <Link to="/my-favorites/edit/:favoritesId">
-        <button>Update category</button>
-        </Link>
+    <Link to={`/my-favorites/edit/${favoritesId}`}>
+      <button>Update category</button>
+    </Link>
 
-        <Link to="/add-pose/:poseId">
-        <button>Add a pose</button>
-        </Link>
+    <Link to={`/add-pose/${poseId}`}>
+      <button>Add a pose</button>
+    </Link>
 
-        <button onClick={() => handleDeleteCategory(favorite.category_id)}>
-          Delete Category
-        </button>
+    <button onClick={() => handleDeleteCategory(favorite.category_id)}>
+      Delete Category
+    </button>
 
-        {/* <button onClick={toggleAddPoseForm}>
-          "Add Pose"
-        </button> */}
+    {/* <button onClick={toggleAddPoseForm}>
+      "Add Pose"
+    </button> */}
 
-        {isAddingPose && (
-          <AddPose
-            onPoseAdded={handleAddPose}
-            // Pass any necessary props to the AddPose component
-          />
-        )}
+    <AddNewPose favoritesId={favoritesId} onPoseAdded={handleAddFavoritePose} />
 
-        {favorite && (
-            <>
-            <h3>{favorite.category_name}</h3>
-            <p>{favorite.category_description}</p>
-            <h4>Poses in {favorite.category_name}</h4>
-            <ul>
-                {favorite.poses &&
-                favorite.poses.map((pose) => (
-                    <li key={pose.id}>
-                    <h5>{pose.english_name}</h5>
-                    <h5>{pose.sanskrit_name}</h5>
-                    <img src={pose.url_svg} alt={pose.english_name} />
-                    <p>{pose.pose_description}</p>
-                    <p>{pose.pose_benefits}</p>
-                    </li>
-                ))}
-            </ul>
-            </>
-        )}
+    {showAddPoseForm && (
+      <div>
+        <AddPose
+          onPoseAdded={handleAddFavoritePose}
+          favoritesId={favoritesId}
+          english_name={english_name}
+          sanskrit_name={sanskrit_name}
+          pose_description={pose_description}
+          pose_benefits={pose_benefits}
+          url_png={url_png}
+        />
 
-        
+        <div className="Add Pose">
+          <h3>Add Pose</h3>
+
+          <form>
+            <label>English Name:</label>
+            <input
+              type="text"
+              name="english_name"
+              value={english_name}
+              onChange={(e) => setEnglishName(e.target.value)}
+            />
+
+            <label>Sanskrit Name:</label>
+            <textarea
+              type="text"
+              name="sanskrit_name"
+              value={sanskrit_name}
+              onChange={(e) => setSanskritName(e.target.value)}
+            />
+
+            <label>Pose Description:</label>
+            <textarea
+              type="text"
+              name="sanskripose_description"
+              value={pose_description}
+              onChange={(e) => setPoseDescription(e.target.value)}
+            />
+
+            <label>Pose Description:</label>
+            <textarea
+              type="text"
+              name="pose_benefits"
+              value={pose_benefits}
+              onChange={(e) => setPoseBenefits(e.target.value)}
+            />
+
+            <label>Pose Description:</label>
+            <textarea
+              type="text"
+              name="url_png"
+              value={url_png}
+              onChange={(e) => setUrlPng(e.target.value)}
+            />        
+
+            <button onClick={handleAddFavoritePose} type="button">
+              Submit
+            </button>
+          </form>
         </div>
-    );
+      </div>
+    )}
+
+    {favorite && (
+      <>
+        <h3>{favorite.category_name}</h3>
+        <p>{favorite.category_description}</p>
+        <h4>Poses in {favorite.category_name}</h4>
+        <ul>
+          {favorite.poses &&
+            favorite.poses.map((pose) => (
+              <li key={pose.id}>
+                <h5>{pose.english_name}</h5>
+                <h5>{pose.sanskrit_name}</h5>
+                <img src={pose.url_svg} alt={pose.english_name} />
+                <p>{pose.pose_description}</p>
+                <p>{pose.pose_benefits}</p>
+              </li>
+            ))}
+        </ul>
+      </>
+    )}
+  </div>
+);
     }
